44037ce0f91ee0b4d5f9e25a0a161c23a1e872ec
GoF9490 <dw9490@naver.com>
2022-08-16 22:54:19 +0900

    0816

--- main.js
+++ main.js
@@ -1,24 +1,22 @@
 let http = require('http');
 let url = require('url');
+let account = require('./lib/account.js');
+
 let app = http.createServer((request, response) =>{
     let _url = request.url;
     let queryData = url.parse(_url, true).query;
     let pathname = url.parse(_url, true).pathname;
     if (pathname === '/'){
-        let html = `
-        <!doctype html>
-        <html>
-            <head>
-                <title>Hello, World</title>
-                <meta charset="utf-8">
-            </head>
-            <body>
-                <h1>Hello, World</h1>
-            </body>
-        </html>
-        `;
-        response.writeHead(200);
-        response.end(html);
+        account.login(request, response);
+    } else if (pathname === '/newAcc'){
+        account.newAcc(request, response);
+    } else if (pathname === '/findId'){
+
+    } else if (pathname === '/findPw'){
+
+    } else {
+        response.writeHead(404);
+        response.end("Not Found");
     }
 });
 app.listen(3000);
