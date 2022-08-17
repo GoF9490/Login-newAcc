ce755d4fdcc13fabdbc4859b55e2c4905a440fbf
GoF9490 <dw9490@naver.com>
2022-08-17 23:29:32 +0900

    0817

--- main.js
+++ main.js
@@ -8,8 +8,12 @@ let app = http.createServer((request, response) =>{
     let pathname = url.parse(_url, true).pathname;
     if (pathname === '/'){
         account.login(request, response);
+    } else if (pathname === '/login_process'){
+        account.login_process(request, response);
     } else if (pathname === '/newAcc'){
         account.newAcc(request, response);
+    } else if (pathname === '/newAcc_process'){
+        account.newAcc_process(request, response);
     } else if (pathname === '/findId'){
 
     } else if (pathname === '/findPw'){
